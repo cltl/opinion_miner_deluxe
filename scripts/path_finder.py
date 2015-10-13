1536ce882260fb4809f06850606af98f3806c69d
Ruben Izquierdo <r.izquierdobevia@vu.nl>
2015-10-13 11:19:01 +0200

    Adding path_finder

---
+++ scripts/path_finder.py
@@ -0,0 +1,12 @@
+
+def get_path_crfsuite():
+    kyoto_path = '/home/izquierdo/bin/crfsuite'
+    return kyoto_path
+
+def get_path_svmlight_learn():
+    kyoto_path = '/home/izquierdo/bin/svm_learn'
+    return kyoto_path
+
+def get_path_svmlight_classify():
+    kyoto_path = '/home/izquierdo/bin/svm_classify'
+    return kyoto_path
